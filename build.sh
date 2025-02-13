9b943d1713aa650d7ccd21f7e4d234cc0cc856c6
Cynu Mini <cynumini@proton.me>
2025-02-13 20:12:44 +0200

    initial commit

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+set -e
+
+bin=hydrus-elo
+
+mkdir -p ./out
+
+case "$1" in
+    run)
+        shift
+        gcc ./src/main.c -o ./out/$bin -std=c99 -Wall -Werror -g -pedantic
+        ./out/$bin $@
+        ;;
+    install)
+        shift
+        target_dir="$1"
+        if [[ -z "$target_dir" ]]; then
+            echo "Usage: $0 install <dir>"
+            exit 1
+        fi
+        echo "Installing to $target_dir"
+        gcc ./src/main.c -o $target_dir/$bin -std=c99 -Wall -Werror -O2 -pedantic
+        ;;
+    *)
+        echo "Usage: $0 {run|install <dir>}"
+        exit 1
+        ;;
+esac
+
